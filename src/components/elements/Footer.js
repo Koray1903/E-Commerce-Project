4628e13d00044d63e5b9532ac4b41f8d711cb5e3
akbakirkoray <59251087+akbakirkoray@users.noreply.github.com>
2020-09-30 23:29:50 +0300

    minor fix

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -45,90 +45,90 @@ const Footer = ({screenWidth}) => {
 
     return (
         <section className="Footer">
-<>
-            {/* CONTACTS */}
-            <div className="Footer__Option__Outer">
-                <p className="Footer__Text">© Copyright {(new Date()).getFullYear()}.<br/>All Rights Reserved.</p>
+            <>
+                {/* CONTACTS */}
+                <div className="Footer__Option__Outer">
+                    <p className="Footer__Text">© Copyright {(new Date()).getFullYear()}.<br/>All Rights Reserved.</p>
 
-                <div className="Footer__Option__Title" onClick={handleContactsClick}>
-                    <p className="Footer__Option__Text">contact</p>
+                    <div className="Footer__Option__Title" onClick={handleContactsClick}>
+                        <p className="Footer__Option__Text">contact</p>
 
-                    {/* ONLY IN MOBILE VERSION */}
-                    {!showContacts ?
-                        <i className={`fas fa-caret-down Footer__Option__Icon__Down`}/>
-                        :
-                        <i className={`fas fa-caret-up Footer__Option__Icon__Up`}/>
-                    }
-                    {/* ONLY IN MOBILE VERSION */}
+                        {/* ONLY IN MOBILE VERSION */}
+                        {!showContacts && mobileVersionBoolean ?
+                            <i className={`fas fa-caret-down Footer__Option__Icon__Down`}/>
+                            :
+                            <i className={`fas fa-caret-up Footer__Option__Icon__Up`}/>
+                        }
+                        {/* ONLY IN MOBILE VERSION */}
+                    </div>
+
+                    <div className="Footer__Option__Inner Footer__Option__Inner__Contacts__Desktop">
+                        <p className={`Footer__Text__Address ${showContacts ? "Element__Display" : "Element__Hide"}`}>
+                            Headquarters:<br/>
+                            5600, Blvd. des Galeries, Bur 530<br/>
+                            Québec, Québec G2K 2H6</p>
+
+                        <a className={`Footer__Text ${showContacts ? "Element__Display" : "Element__Hide"}`}
+                           href="mailto:contact@osf-global.com">contact@osf-global.com</a>
+
+                        <a className={`Footer__Text Footer__Text__OnHover ${showContacts ? "Element__Display" : "Element__Hide"}`}
+                           href="tel:+1-888-548-4344">+1 (888) 548-4344</a>
+                    </div>
                 </div>
+                {/* CONTACTS */}
 
-                <div className="Footer__Option__Inner Footer__Option__Inner__Contacts__Desktop">
-                    <p className={`Footer__Text__Address ${showContacts ? "Element__Display" : "Element__Hide"}`}>
-                        Headquarters:<br/>
-                        5600, Blvd. des Galeries, Bur 530<br/>
-                        Québec, Québec G2K 2H6</p>
 
-                    <a className={`Footer__Text ${showContacts ? "Element__Display" : "Element__Hide"}`}
-                       href="mailto:contact@osf-global.com">contact@osf-global.com</a>
+                {/* CATEGORIES */}
+                <div className="Footer__Option__Outer">
+                    <div className="Footer__Option__Title" onClick={handleCategoriesClick}>
+                        <p className="Footer__Option__Text">categories</p>
 
-                    <a className={`Footer__Text Footer__Text__OnHover ${showContacts ? "Element__Display" : "Element__Hide"}`}
-                       href="tel:+1-888-548-4344">+1 (888) 548-4344</a>
+                        {/* ONLY IN MOBILE VERSION */}
+                        {!showCategories && mobileVersionBoolean ?
+                            <i className={`fas fa-caret-down Footer__Option__Icon__Down`}/>
+                            :
+                            <i className={`fas fa-caret-up Footer__Option__Icon__Up`}/>
+                        }
+                        {/* ONLY IN MOBILE VERSION */}
+                    </div>
+
+                    <div className="Footer__Option__Inner Footer__Option__Inner__Categories__Desktop">
+                        {categories.map((element, index) => (
+                            <Link to="/404" className="" key={index}>
+                                <p className={`Footer__Text ${showCategories ? "Element__Display" : "Element__Hide"}`}>{element}</p>
+                            </Link>
+                        ))}
+                    </div>
                 </div>
-            </div>
-            {/* CONTACTS */}
+                {/* CATEGORIES */}
 
 
-            {/* CATEGORIES */}
-            <div className="Footer__Option__Outer">
-                <div className="Footer__Option__Title" onClick={handleCategoriesClick}>
-                    <p className="Footer__Option__Text">categories</p>
+                {/* ABOUT */}
+                <div className="Footer__Option__Outer">
+                    <div className="Footer__Option__Title" onClick={handleAboutClick}>
+                        <p className="Footer__Option__Text">about</p>
 
-                    {/* ONLY IN MOBILE VERSION */}
-                    {!showCategories ?
-                        <i className={`fas fa-caret-down Footer__Option__Icon__Down`}/>
-                        :
-                        <i className={`fas fa-caret-up Footer__Option__Icon__Up`}/>
-                    }
-                   {/* ONLY IN MOBILE VERSION */}
+                        {/*ONLY IN MOBILE VERSION*/}
+                        {!showAbout && mobileVersionBoolean ?
+                            <i className={`fas fa-caret-down Footer__Option__Icon__Down`}/>
+                            :
+                            <i className={`fas fa-caret-up Footer__Option__Icon__Up`}/>
+                        }
+                        {/*ONLY IN MOBILE VERSION*/}
+                    </div>
+
+                    <div className="Footer__Option__Inner Footer__Option__Inner__About__Desktop">
+                        {about.map((element, index) => (
+                            <Link to="/404" className="" key={index}>
+                                <p className={`Footer__Text ${showAbout ? "Element__Display" : "Element__Hide"}`}>{element}</p>
+                            </Link>
+                        ))}
+                    </div>
                 </div>
+                {/* ABOUT */}
 
-                <div className="Footer__Option__Inner Footer__Option__Inner__Categories__Desktop">
-                    {categories.map((element, index) => (
-                        <Link to="/404" className="" key={index}>
-                            <p className={`Footer__Text ${showCategories ? "Element__Display" : "Element__Hide"}`}>{element}</p>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-            {/* CATEGORIES */}
-
-
-            {/* ABOUT */}
-            <div className="Footer__Option__Outer">
-                <div className="Footer__Option__Title" onClick={handleAboutClick}>
-                    <p className="Footer__Option__Text">about</p>
-
-                    {/*ONLY IN MOBILE VERSION*/}
-                    {!showAbout ?
-                        <i className={`fas fa-caret-down Footer__Option__Icon__Down`}/>
-                        :
-                        <i className={`fas fa-caret-up Footer__Option__Icon__Up`}/>
-                    }
-                    {/*ONLY IN MOBILE VERSION*/}
-                </div>
-
-                <div className="Footer__Option__Inner Footer__Option__Inner__About__Desktop">
-                    {about.map((element, index) => (
-                        <Link to="/404" className="" key={index}>
-                            <p className={`Footer__Text ${showAbout ? "Element__Display" : "Element__Hide"}`}>{element}</p>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-            {/* ABOUT */}
-
-            <SocialNetworkLogos/>
-</>
+                <SocialNetworkLogos/>
+            </>
         </section>
     );
 };
